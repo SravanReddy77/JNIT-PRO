52d2df1d70fdd17869f125cc4f4b86fef2fd16fb
SravanReddy77 <sravanreddy.annadi7777@gmail.com>
2023-11-13 13:11:08 -0600

    added login and sign up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          1231 <code>Greenway Drive</code> Irving, Texas. 75038.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.google.com/maps/place/1231+Greenway+Dr,+Irving,+TX+75038/@32.8900989,-96.9708748,17z/data=!3m1!4b1!4m6!3m5!1s0x864c29d4094ce497:0x4dfcf083c038301c!8m2!3d32.8900944!4d-96.9682999!16s%2Fg%2F11bw3ygskg?entry=ttu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Location
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           1231 <code>Greenway Drive</code> Irving, Texas. 75038.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://www.google.com/maps/place/1231+Greenway+Dr,+Irving,+TX+75038/@32.8900989,-96.9708748,17z/data=!3m1!4b1!4m6!3m5!1s0x864c29d4094ce497:0x4dfcf083c038301c!8m2!3d32.8900944!4d-96.9682999!16s%2Fg%2F11bw3ygskg?entry=ttu"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Location
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
