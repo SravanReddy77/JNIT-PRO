de1ae0e738d60c88d46b48fdff0989c4cf834310
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2023-12-01 11:52:13 -0600

    Pull

--- client/src/createNewPassword.js
+++ client/src/createNewPassword.js
@@ -11,34 +11,6 @@ export default function CreateNewPassword(){
                 <a href="http://localhost:3000/">
                     <img src={JnitLogo} className="center" alt="JnitLogo"></img>
                 </a>
-                <div className="Auth-form-container-forgotPassword">
-                    <form className="Auth-form">
-                        <div className="Auth-form-content">
-                            <h3 className="Auth-form-title">Create New Password</h3>
-                            <div className="form-group mt-3">
-                                <label>Enter Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control mt-1"
-                                    placeholder="Enter New Password"
-                                />
-                                <label>Renter Password</label>
-                                <input
-                                    type="Reenter password"
-                                    className="form-control mt-2"
-                                    placeholder="Reenter New Password"
-                                />
-                            </div>
-
-                            <div className="d-grid gap-2 mt-3">
-                                <button type="submit" className="btn btn-primary" >
-                                    Send OTP
-                                </button>
-
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
 
         )
