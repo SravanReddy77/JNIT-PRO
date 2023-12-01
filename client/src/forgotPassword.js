0022682bd4f19137817cc96536d749069616e067
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2023-12-01 11:31:56 -0600

    pull

--- client/src/forgotPassword.js
+++ client/src/forgotPassword.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import JnitLogo from "./JNIT Logo.svg";
 import "./login.css";
+import {redirect} from "react-router";
+import CreateNewPassword from "./createNewPassword";
 
 export default function ForgotPassword(){
+    let [authMode, setAuthMode] = useState("forgotPassword")
+    function handleSubmit(){
+        redirect("./createNewPassword")
+    }
+
+    const changePassMode = () => {
+        setAuthMode(authMode === "forgotPassword" ? "enterOTP" : "forgotPassword")
+    }
+        if (authMode === "forgotPassword") {
+        return (
+            <div className="Title">
+                <h1>&nbsp;JNIT<a href="http://localhost:3000/"><img src={JnitLogo} height={70} width={70}
+                                                                    alt="JnitLogo"></img></a></h1>
+                <div className="header">Pro Members</div>
+                <a href="http://localhost:3000/">
+                    <img src={JnitLogo} className="center" alt="JnitLogo"></img>
+                </a>
+                <div className="Auth-form-container-forgotPassword">
+                    <form className="Auth-form">
+                        <div className="Auth-form-content">
+                            <h3 className="Auth-form-title">Forgot Password?</h3>
+                            <div className="form-group mt-3">
+                                <label>Enter Email</label>
+                                <input
+                                    type="email"
+                                    className="form-control mt-1"
+                                    placeholder="Enter email"
+                                />
+                            </div>
+
+                            <div className="d-grid gap-2 mt-3">
+                                <button type="submit" className="btn btn-primary" onClick={changePassMode}>
+                                    Send OTP
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        )
+    }
     return (
         <div className="Title">
-            <h1>&nbsp;JNIT<a href="http://localhost:3000/"><img src={JnitLogo} height={70} width={70} alt="JnitLogo"></img></a></h1>
+            <h1>&nbsp;JNIT<a href="http://localhost:3000/"><img src={JnitLogo} height={70} width={70}
+                                                                alt="JnitLogo"></img></a></h1>
             <div className="header">Pro Members</div>
             <a href="http://localhost:3000/">
                 <img src={JnitLogo} className="center" alt="JnitLogo"></img>
             </a>
-            <div className="Auth-form-container">
+            <div className="Auth-form-container-forgotPassword">
                 <form className="Auth-form">
                     <div className="Auth-form-content">
                         <h3 className="Auth-form-title">Forgot Password?</h3>
@@ -21,24 +67,25 @@ export default function ForgotPassword(){
                                 className="form-control mt-1"
                                 placeholder="Enter email"
                             />
-                        </div>
-                        <div className="form-group mt-3">
-                            <label>Reenter Email</label>
+                            <br/>
+                            <label>Enter OTP</label>
                             <input
-                                type="email"
-                                className="form-control mt-1"
-                                placeholder="Reenter email"
+                                type="enter OTP"
+                                className="form-control mt-2"
+                                placeholder="Enter OTP"
                             />
                         </div>
-
-                        <div className="d-grid gap-2 mt-3">
-                            <button type="submit" className="btn btn-primary">
-                                Send OTP
-                            </button>
-                        </div>
+                        <form action="./classNewPassword">
+                            <div className="d-grid gap-2 mt-3">
+                                <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+                                Submit
+                                 </button>
+                            </div>
+                        </form>
                     </div>
                 </form>
             </div>
         </div>
+
     )
 }
