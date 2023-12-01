5c629297d71e01930da953800e94b0bbe2f6ed40
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2023-12-01 12:55:25 -0600

    All Pages

--- client/src/landingPage.js
+++ client/src/landingPage.js
@@ -4,12 +4,9 @@ import JnitLogo from "./JNIT Logo.svg"
 export default function LandingPage(){
 
     return (
-        <div className="Title">
-            <h1>&nbsp;JNIT<a href="http://localhost:3000/"><img  src={JnitLogo} height={70} width={70} alt="JnitLogo"></img></a></h1>
+        <div className="Title" className="header">
+            <h3>&nbsp;JNIT<a href="http://localhost:3000/"><img  src={JnitLogo} height={30} width={30} alt="JnitLogo"></img></a></h3>
 
-            <div className="header">Pro Members</div>
-
-              <h1  align="center">Welcome to JNIT</h1>
             <a href="./HireEmployee">Link</a>
 
               </div>
