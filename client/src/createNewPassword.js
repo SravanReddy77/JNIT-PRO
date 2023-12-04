69693e313dfa8ef466392b0093f130f5676c33bf
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2023-12-03 21:31:06 -0600

    logo

--- client/src/createNewPassword.js
+++ client/src/createNewPassword.js
@@ -12,9 +12,11 @@ export default function CreateNewPassword(){
             <div className="header">
                 <h3>&nbsp;JNIT<a href="http://localhost:3000/"><img  src={JnitLogo} height={30} width={30} alt="JnitLogo"></img></a></h3>
             </div>
-                <a href="http://localhost:3000/">
-                    <img src={JnitLogo} className="center" alt="JnitLogo"></img>
-                </a>
+                <div className="image">
+                    <a href="http://localhost:3000/">
+                        <img src={JnitLogo} className="center" alt="JnitLogo"></img>
+                    </a>
+                </div>
                 <div className="Auth-form-container-createNewpassword">
                     <form className="Auth-form">
                         <div className="Auth-form-content">
--- client/src/register.js
+++ client/src/register.js
@@ -19,10 +19,12 @@ export default function Register (props) {
         <div>
         <div className="header">
           <h3>&nbsp;JNIT<a href="http://localhost:3000/"><img  src={JnitLogo} height={30} width={30} alt="JnitLogo"></img></a></h3>
-            </div>
+        </div>
+          <div className="image">
           <a href="http://localhost:3000/">
             <img src={JnitLogo} className="center" alt="JnitLogo"></img>
           </a>
+          </div>
       <div className="Auth-form-container-signin">
         <form className="Auth-form">
           <div className="Auth-form-content">
@@ -79,9 +81,11 @@ export default function Register (props) {
     <div className="header">
       <h3>&nbsp;JNIT<a href="http://localhost:3000/"><img  src={JnitLogo} height={30} width={30} alt="JnitLogo"></img></a></h3>
         </div>
-      <a href="http://localhost:3000/">
-      <img src={JnitLogo} className="center" alt="JnitLogo"></img>
-    </a>
+      <div className="image">
+        <a href="http://localhost:3000/">
+          <img src={JnitLogo} className="center" alt="JnitLogo"></img>
+        </a>
+      </div>
     <div className="Auth-form-container">
       <form className="Auth-form">
         <div className="Auth-form-content">
