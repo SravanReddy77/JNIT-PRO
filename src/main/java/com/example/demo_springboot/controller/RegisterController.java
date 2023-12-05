ea45bd272bbac7149a7817d360d2855a2c79f35c
SravanReddy77 <sravanreddy.annadi7777@gmail.com>
2023-12-05 11:01:34 -0600

    Rest-API's

--- src/main/java/com/example/demo_springboot/controller/RegisterController.java
+++ src/main/java/com/example/demo_springboot/controller/RegisterController.java
@@ -1,20 +1,40 @@
 package com.example.demo_springboot.controller;
 
-import com.example.demo_springboot.model.Register;
+import com.example.demo_springboot.model.Registration;
 import com.example.demo_springboot.repository.RegisterRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class RegisterController {
 
     @Autowired
-    RegisterRepository registerRepository;
+    private RegisterRepository registerRepository;
+
     @PostMapping("/saveRegister")
-    public Register saveRegister(@RequestBody Register register) {
+    public Registration saveRegister(@RequestBody Registration register) {
 
         return registerRepository.save(register);
     }
+
+    @GetMapping("/getRegister")
+    public List<Registration> getRegister() {
+        return registerRepository.findAll();
+    }
+
+    
+    @DeleteMapping("/deleteRegister")
+    public void deleteRegisterByID(@RequestBody Registration register) {
+
+         registerRepository.delete(register);
+    }
+
+    
 }
