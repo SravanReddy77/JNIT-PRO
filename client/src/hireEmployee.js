3e3afddc32d89e479725fd4635479d35abd6c167
manu <manudeepvugge@gmail.com>
2023-12-05 12:57:07 -0600

    view details tab

--- client/src/hireEmployee.js
+++ client/src/hireEmployee.js
@@ -17,36 +17,26 @@ export default function HireEmployee() {
                 <h2> John Hooper</h2>
                 <p> Sr.Full Stack Java Developer</p>
                 <p>Details: 1500$/month,<br></br>Client : Neilson(Chicago)<br></br>
-                             USA </p><br></br>
-                             <form>
-                                <input type="submit" value="view profile" ></input>
-                             </form>
+                             USA </p>
+                             <button type="button" className="view-details-btn">View Details</button>
               
                  </div>
             <div className="employee-box">
                 <img src={employee} alt="Employee 2" className="employee-image"></img>
                 <h2> James Richardson</h2>
                 <p> Python </p>
-<<<<<<< Updated upstream
                 <p>Details:3000$/month, <br></br>Client : Macys(Texas)<br></br>
                     India </p>
-=======
-                <p>Details: </p>
-
->>>>>>> Stashed changes
+                    <button type="button" className="view-details-btn">View Details</button>
             </div>
 
             <div className="employee-box">
                 <img src={employee} alt="Employee 3" className="employee-image"></img>
                 <h2> Kinney William </h2>
                 <p> Devops </p>
-<<<<<<< Updated upstream
                 <p>Details:5000$/month,<br></br>Client: Amazon(Boston)<br></br>
                   USA </p>
-=======
-                <p>Details: </p>
                 <button type="button" className="view-details-btn">View Details</button>
->>>>>>> Stashed changes
             </div>
             <div className="employee-box">
                 <img src={employee} alt="Employee 4" className="employee-image"></img>
@@ -55,6 +45,7 @@ export default function HireEmployee() {
                 <p>Details: 1300$/month,<br></br>Client : Costco(NewYork)<br></br>
                     China
                 </p>
+                <button type="button" className="view-details-btn">View Details</button>
             </div>
         </div>
     )
