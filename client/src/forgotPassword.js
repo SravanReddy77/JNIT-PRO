8479406536f4b93c1c16f0f2647cff8e42ac652e
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2024-01-09 11:10:17 -0600

    Nav:Login to HireEmployee

--- client/src/forgotPassword.js
+++ client/src/forgotPassword.js
@@ -3,6 +3,7 @@ import JnitLogo from "./JNIT Logo.svg";
 import "./login.css";
 import {redirect} from "react-router";
 import NavbarComponent from "./Navigation";
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from "reactstrap";
 
 export default function ForgotPassword(){
     let [authMode, setAuthMode] = useState("forgotPassword")
@@ -13,10 +14,43 @@ export default function ForgotPassword(){
     const changePassMode = () => {
         setAuthMode(authMode === "forgotPassword" ? "enterOTP" : "forgotPassword")
     }
+    const [collapsed, setCollapsed] = useState(true);
+    const toggleNavbar = () => setCollapsed(!collapsed);
         if (authMode === "forgotPassword") {
         return (
             <div>
-                <NavbarComponent/>
+                <div>
+                    <Navbar color="faded" dark>
+                        <NavbarBrand href="/" className="me-0">
+                        </NavbarBrand>
+                        <NavbarBrand className="me-auto">
+                            <NavbarToggler onClick={toggleNavbar}/>
+                            <a href="http://localhost:3000/"><img src={JnitLogo} height={40} width={80}
+                                                                  alt="JnitLogo"></img></a>
+                            <Collapse isOpen={!collapsed} navbar>
+                                <Nav navbar>
+                                    <NavItem>
+                                        <NavLink href="./">
+                                            Home
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink href="./hireEmployee">
+                                            Login In
+                                        </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink href="/components/">Contact Us</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink href="./">
+                                            About
+                                        </NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </Collapse></NavbarBrand>
+                    </Navbar>
+                </div>
                 <div className="login">
                     <a href="http://localhost:3000/">
                         <img src={JnitLogo} className="center" alt="JnitLogo"></img>
@@ -46,21 +80,53 @@ export default function ForgotPassword(){
             </div>
 
         )
-    }
+        }
     return (
         <div>
-            <NavbarComponent/>
+            <div>
+                <Navbar color="faded" dark>
+                    <NavbarBrand href="/" className="me-0">
+                    </NavbarBrand>
+                    <NavbarBrand className="me-auto">
+                        <NavbarToggler onClick={toggleNavbar}/>
+                        <a href="http://localhost:3000/"><img src={JnitLogo} height={40} width={80}
+                                                              alt="JnitLogo"></img></a>
+                        <Collapse isOpen={!collapsed} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink href="./">
+                                        Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="./hireEmployee">
+                                        Login In
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/components/">Contact Us</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="./">
+                                        About
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse></NavbarBrand>
+                </Navbar>
+            </div>
 
-                <div className="login">
-                    <a href="http://localhost:3000/">
-                        <img src={JnitLogo} className="center" alt="JnitLogo"></img>
-                    </a>
+            <div className="login">
+                <a href="http://localhost:3000/">
+                    <img src={JnitLogo} className="center" alt="JnitLogo"></img>
+                </a>
 
-                    <h2>OTP</h2>
+                <h2>OTP</h2>
                 <form className="login-form">
                     <div className="textbox">
 
-                        <p className="Auth-form-confirm" align="center"><span className="greenText">Sent Successfully!</span></p>
+                        <p className="Auth-form-confirm" align="center"><span
+                            className="greenText">Sent Successfully!</span></p>
                         <div className="form-group mt-3">
                             <label className="custom-label">Enter Email</label>
                             <input
@@ -75,14 +141,14 @@ export default function ForgotPassword(){
                                 type="enter OTP"
                                 className="form-control mt-2"
                                 placeholder="Enter OTP"
-                              
+
                             />
                         </div>
                         <form className="d-grid gap-2 mt-3" action="./createNewPassword">
-                        <div className="d-grid gap-2 mt-3">
+                            <div className="d-grid gap-2 mt-3">
                                 <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
-                                Submit
-                                 </button>
+                                    Submit
+                                </button>
                             </div>
                         </form>
                     </div>
--- client/src/register.js
+++ client/src/register.js
@@ -4,7 +4,7 @@ import "./login.css"
 import JnitLogo from './JNIT Logo.svg';
 import { redirect } from "react-router";
 import axios from "axios";
-import NavbarComponent from "./Navigation";
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from "reactstrap";
 
 
 
@@ -67,17 +67,53 @@ const registerData = () => {
     });
 }
   function handleSubmit(){
-    redirect("./landing")
+  console.log("Hi")
+    redirect("./hireEmployee")
   }
 
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin")
   }
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
 
   if (authMode === "signin") {
     return (
         <div>
-        <NavbarComponent/>
+ {/*       <NavbarComponent/>*/}
+          <div>
+            <Navbar color="faded" dark>
+              <NavbarBrand href="/" className="me-0">
+              </NavbarBrand>
+              <NavbarBrand className="me-auto">
+                <NavbarToggler onClick={toggleNavbar}/>
+                  <a href="http://localhost:3000/"><img src={JnitLogo} height={40} width={80}
+                                                    alt="JnitLogo"></img></a>
+                  <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                      <NavItem>
+                        <NavLink href="./">
+                          Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="./hireEmployee">
+                        Consultants
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="/components/">Contact Us</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="./">
+                        About
+                      </NavLink>
+                    </NavItem>
+                </Nav>
+              </Collapse></NavbarBrand>
+            </Navbar>
+          </div>
+
           <div className="login">
               <div className="image">
                 <a href="http://localhost:3000/">
@@ -106,7 +142,7 @@ const registerData = () => {
                         required
                       />
                   </div>
-                  <form className="d-grid gap-2 mt-3" action="./landing">
+                  <form className="d-grid gap-2 mt-3">
                       <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
                         Login
                       </button>
@@ -114,12 +150,12 @@ const registerData = () => {
                   <p className="text-center mt-2">
                     <a className="custom-label" href="./ForgotPassword"> Forgot password?</a>
                   </p>
-                  <a className="custom-label">
+                  <div className="custom-label">
                   Not registered yet?{" "}
                   <span className="link-primary" onClick={changeAuthMode}>
                 Sign Up
               </span>
-                  </a>
+                  </div>
                 </div>
               </form>
           </div>
@@ -129,82 +165,114 @@ const registerData = () => {
   }
 
   return (
-    <div>
-      <NavbarComponent/>
-      <div className="login">
-        <a href="http://localhost:3000/">
-          <img src={JnitLogo} className="center" alt="JnitLogo"></img>
-        </a>
-        <h1>Sign Up</h1>
-      <form className="login-form">
-        <div className="textbox">
-          <div className="dropdown-container">
-            <label className="custom-label" htmlFor="dropdown">SignUp: </label>
-            <select id="dropdown" className="custom-dropdown" onChange= {handleUserTypeChange} value={userType}>
-              <option value="" disabled selected>Select an option</option>
-              <option value="ADMIN">Admin</option>
-              <option value="JUNIOR_ADMIN">Junior Admin</option>
-              <option value="SUPER_ADMIN">Senior Admin</option>
-            </select>
-          </div>
-          <div className="form-group mt-1">
-            <label className="custom-label">Full Name</label>
-            <input
-              type="text"
-              className="form-control mt-1"
-              placeholder="Full Name"
-              onChange={handleNameChange}
-              value={name}
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label className="custom-label">Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
-              onChange={handleEmailChange}
-              value={email}
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label className="custom-label">Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter Password"
-              onChange={handlePasswordChange}
-              value={password}
-            />
-          </div>
-            <div className="form-group mt-3">
-            <label className="custom-label">Reenter Password</label>
-            <input
-              type="password"
-              value={password}
-              className="form-control mt-1"
-              placeholder="Reenter Password"
-              onChange={(e) => setPassword(e.target.value)}
-              />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary" onClick={registerData}>
-              Submit
-            </button>
-            <div className="text-center">
-              <div className="custom-label">
-              Already registered?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-              Sign In
-            </span>
-              </div>
-            </div>
-          </div>
-
+      <div>
+        <div>
+          <Navbar color="faded" dark>
+            <NavbarBrand href="/" className="me-0">
+            </NavbarBrand>
+            <NavbarBrand className="me-auto">
+              <NavbarToggler onClick={toggleNavbar}/>
+              <a href="http://localhost:3000/"><img src={JnitLogo} height={40} width={80}
+                                                    alt="JnitLogo"></img></a>
+              <Collapse isOpen={!collapsed} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink href="./">
+                      Home
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="./hireEmployee">
+                     Login In
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/components/">Contact Us</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="./">
+                      About
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse></NavbarBrand>
+          </Navbar>
         </div>
-      </form>
-   {/* </div>*/}
-    </div>
-    </div>
+
+        <div className="login">
+          <a href="http://localhost:3000/">
+            <img src={JnitLogo} className="center" alt="JnitLogo"></img>
+          </a>
+          <h1>Sign Up</h1>
+          <form className="login-form">
+            <div className="textbox">
+              <div className="dropdown-container">
+                <label className="custom-label" htmlFor="dropdown">SignUp: </label>
+                <select id="dropdown" className="custom-dropdown" onChange={handleUserTypeChange} value={userType}>
+                  <option value="" disabled selected>Select an option</option>
+                  <option value="ADMIN">Admin</option>
+                  <option value="JUNIOR_ADMIN">Junior Admin</option>
+                  <option value="SUPER_ADMIN">Senior Admin</option>
+                </select>
+              </div>
+              <div className="form-group mt-1">
+                <label className="custom-label">Full Name</label>
+                <input
+                    type="text"
+                    className="form-control mt-1"
+                    placeholder="Full Name"
+                    onChange={handleNameChange}
+                    value={name}
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label className="custom-label">Email address</label>
+                <input
+                    type="email"
+                    className="form-control mt-1"
+                    placeholder="Email Address"
+                    onChange={handleEmailChange}
+                    value={email}
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label className="custom-label">Password</label>
+                <input
+                    type="password"
+                    className="form-control mt-1"
+                    placeholder="Enter Password"
+                    onChange={handlePasswordChange}
+                    value={password}
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label className="custom-label">Reenter Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    className="form-control mt-1"
+                    placeholder="Reenter Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-primary" onClick={registerData}>
+                  Submit
+                </button>
+                <div className="text-center">
+                  <div className="custom-label">
+                    Already registered?{" "}
+                    <span className="link-primary" onClick={changeAuthMode}>
+              Login In
+            </span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </form>
+          {/* </div>*/}
+        </div>
+      </div>
   )
 }
