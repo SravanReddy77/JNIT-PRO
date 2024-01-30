0cf42169179ec361841c06da64cda8df40fe07a2
Sanath Kumar Ankala <100515204+ASKR04@users.noreply.github.com>
2024-01-30 11:49:06 -0600

    Created  Home Page

---
+++ client/src/Home.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./login.css"
+import JnitLogo from './JNIT Logo.svg';
+import axios from "axios";
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from "reactstrap";
